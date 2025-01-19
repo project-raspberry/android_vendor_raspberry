ece70ba24b0501a8c348182c4da998ec3e07a26e
winbo.yml <winbo.yml@proton.me>
2025-01-19 12:38:29 +0100

    otapackage.mk: We are Project Raspberry

--- build/tasks/otapackage.mk
+++ build/tasks/otapackage.mk
@@ -1,6 +1,7 @@
 # Copyright (C) 2017 Unlegacy-Android
 # Copyright (C) 2017,2020 The LineageOS Project
 # Copyright (C) 2024 risingOS
+# Copyright (C) 2025 Project Raspberry
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,38 +16,38 @@
 # limitations under the License.
 
 # -----------------------------------------------------------------
-# RisingOS OTA update package
+# Project Raspberry OTA update package
 
-RISING_TARGET_PACKAGE := $(PRODUCT_OUT)/RisingOS-$(RISING_BUILD_VERSION)-ota.zip
+RASPBERRY_TARGET_PACKAGE := $(PRODUCT_OUT)/Project-Raspberry-15-ota.zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(RISING_TARGET_PACKAGE)
-	$(hide) $(SHA256) $(RISING_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(RISING_TARGET_PACKAGE).sha256sum
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(RASPBERRY_TARGET_PACKAGE)
+	$(hide) $(SHA256) $(RASPBERRY_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(RASPBERRY_TARGET_PACKAGE).sha256sum
 	@echo ""
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "  ______ _____ _______ _____ __   _  ______      _____  _______" >&2
-	@echo " |_____/   |   |______   |   | \  | |  ____     |     | |______" >&2
-	@echo " |    \_ __|__ ______| __|__ |  \_| |_____|     |_____| ______|" >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "                   rising from the bottom                      " >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
-	@echo "                                                               " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
+	@echo "██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗    ██████╗  █████╗ ███████╗██████╗ ██████╗ ███████╗██████╗ ██████╗ ██╗   ██╗" >&2
+	@echo "██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝    ██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗ ██╔╝" >&2
+	@echo "██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║       ██████╔╝███████║███████╗██████╔╝██████╔╝█████╗  ██████╔╝██████╔╝ ╚████╔╝ " >&2
+	@echo "██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║       ██╔══██╗██╔══██║╚════██║██╔═══╝ ██╔══██╗██╔══╝  ██╔══██╗██╔══██╗  ╚██╔╝  " >&2
+	@echo "██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║       ██║  ██║██║  ██║███████║██║     ██████╔╝███████╗██║  ██║██║  ██║   ██║   " >&2
+	@echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝       ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
+	@echo "                                                               									      " >&2
 	@echo ""
-	@echo "Creating json OTA..." >&2
-	$(hide) ./vendor/rising/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) RisingOS-$(RISING_BUILD_VERSION)-ota.zip $(RISING_VERSION) $(RISING_CODENAME) $(RISING_PACKAGE_TYPE) $(RISING_RELEASE_TYPE)
-	$(hide) cp -f $(PRODUCT_OUT)/$(RISING_PACKAGE_TYPE)_$(TARGET_DEVICE).json vendor/risingOTA/$(RISING_PACKAGE_TYPE)_$(TARGET_DEVICE).json
+	@echo "Creating JSON OTA..." >&2
+	$(hide) ./vendor/raspberry/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) Project-Raspberry-15-ota.zip $(RASPBERRY_VERSION) $(RASPBERRY_CODENAME) $(RASPBERRY_PACKAGE_TYPE) $(RASPBERRY_RELEASE_TYPE)
+	$(hide) cp -f $(PRODUCT_OUT)/$(RASPBERRY_PACKAGE_TYPE)_$(TARGET_DEVICE).json vendor/RaspberryOTA/$(RASPBERRY_PACKAGE_TYPE)_$(TARGET_DEVICE).json
 	@echo ":·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·:" >&2
-	@echo " Size            : $(shell du -hs $(RISING_TARGET_PACKAGE) | awk '{print $$1}')"
-	@echo " Size(in bytes)  : $(shell wc -c $(RISING_TARGET_PACKAGE) | awk '{print $$1}')"
-	@echo " Package Complete: $(RISING_TARGET_PACKAGE)" >&2
+	@echo " Size            : $(shell du -hs $(RASPBERRY_TARGET_PACKAGE) | awk '{print $$1}')"
+	@echo " Size(in bytes)  : $(shell wc -c $(RASPBERRY_TARGET_PACKAGE) | awk '{print $$1}')"
+	@echo " Package Complete: $(RASPBERRY_TARGET_PACKAGE)" >&2
 	@echo ":·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·:" >&2
 	@echo ""
